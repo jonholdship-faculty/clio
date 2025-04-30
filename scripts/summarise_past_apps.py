5cffbb859d7651db190e45395401cc5230cd1ba5
Jon Holdship <jonathan.holdship@faculty.ai>
2025-04-30 16:18:33 +0100

    full api

--- scripts/summarise_past_apps.py
+++ scripts/summarise_past_apps.py
@@ -1,9 +1,31 @@
 from clio.summary.openai_summariser import OpenAISummariser
 import pandas as pd
 
-df = pd.read_csv("data/Planning Hack Data v0.1.csv")
-df = df[["Address", "Application Number", "FullText"]].dropna()
+df = pd.read_csv("data/Planning Hack Data v0.1.csv", quotechar='"')
+df = df[["Address", "Application Number", "FullText", "Outcome"]].dropna()
+
+df2 = pd.read_excel("data/Planning Hack Data v0.2.xlsx")
+df2 = df2[["Address", "Application Number", "FullText", "Outcome"]].dropna()
+
+df = pd.concat([df, df2]).reset_index(drop=True)
+df = df.rename(
+    {
+        "Address": "address",
+        "Application Number": "application_number",
+        "FullText": "full_text",
+        "Outcome": "outcome",
+    },
+    axis=1,
+)
+
 summariser = OpenAISummariser()
 
-df["reason"] = df["FullText"].map(summariser.summarise_document)
+df["reason"] = df.apply(
+    lambda row: summariser.summarise_document(
+        document_text=row["full_text"], outcome=row["outcome"]
+    ),
+    axis=1,
+)
+print(df)
+
 df.to_parquet("data/website_data.pq")
--- src/clio/api/api.py
+++ src/clio/api/api.py
@@ -11,35 +11,21 @@ data = pd.read_parquet("data/website_data.pq")
 
 @app.get("/list/{query_str}", response_model=list[str])
 def list_addresses(query_str: str) -> list[str]:
-    idx = data["Address"].str.lower().str.contains(query_str.lower())
-    return data.loc[idx, "Address"].unique()
+    idx = data["address"].str.lower().str.contains(query_str.lower())
+    return data.loc[idx, "address"].unique()
 
 
 @app.get("/address/{address}", response_model=list[ApplicationSummary])
 def query_address(address: str):
+    rows = data.loc[data["address"].str.lower().str.contains(address.lower())]
     return [
         ApplicationSummary(
             decision_date=date.fromisoformat("2023-01-01"),
-            application_ref="test1",
-            address=address,
-            type="application",
-            decision_type="refusal",
-            reason="test case 1",
-        ),
-        ApplicationSummary(
-            decision_date=date.fromisoformat("2023-01-01"),
-            application_ref="test2",
-            address=address,
-            type="appeal",
-            decision_type="appeal_allowed",
-            reason="test case 2",
-        ),
-        ApplicationSummary(
-            decision_date=date.fromisoformat("2023-01-01"),
-            application_ref="test3",
-            address=address,
-            type="application",
-            decision_type="approval",
-            reason="test case 3",
-        ),
+            application_ref=row["application_number"],
+            address=row["address"],
+            type="test type",
+            decision_type=row["outcome"],
+            reason=row["reason"],
+        )
+        for _, row in rows.iterrows()
     ]
